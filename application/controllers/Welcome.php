1d733d8fdde7bd79621915e8beddbebb23ff2996
novinaldi <novinaldi1309@gmail.com>
2019-12-15 21:23:18 +0700

    Menambahkan Proses Import Dari Excel Ke Database MySQL-Fix

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,83 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library(array('PHPExcel', 'PHPExcel/IOFactory'));
+	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('formimport');
+	}
+
+	function doimport()
+	{
+		$jmlsukses = 0;
+		$jmlgagal = 0;
+		$fileName = $_FILES['uploadfile']['name'];
+
+		$config['upload_path'] = './assets/fileimport/'; //buat folder dengan nama assets di root folder
+		$config['file_name'] = $fileName;
+		$config['allowed_types'] = 'xls|xlsx';
+		$config['max_size'] = 10000;
+
+		$this->load->library('upload');
+		$this->upload->initialize($config);
+
+		if (!$this->upload->do_upload('uploadfile')) {
+			echo $this->upload->display_errors();
+		} else {
+			$media = $this->upload->data();
+			$inputFileName = './assets/fileimport/' . $media['file_name'];
+
+			try {
+				$inputFileType = IOFactory::identify($inputFileName);
+				$objReader = IOFactory::createReader($inputFileType);
+				$objPHPExcel = $objReader->load($inputFileName);
+			} catch (Exception $e) {
+				die('Error loading file "' . pathinfo($inputFileName, PATHINFO_BASENAME) . '": ' . $e->getMessage());
+			}
+
+			$sheet = $objPHPExcel->getSheet(0);
+			$highestRow = $sheet->getHighestRow();
+			$highestColumn = $sheet->getHighestColumn();
+
+			for ($row = 2; $row <= $highestRow; $row++) {                  //  Read a row of data into an array                 
+				$rowData = $sheet->rangeToArray(
+					'A' . $row . ':' . $highestColumn . $row,
+					NULL,
+					TRUE,
+					FALSE
+				);
+
+				$nim = $rowData[0][1];
+				$nama = $rowData[0][2];
+				$jenkel = $rowData[0][3];
+				$tmplahir = $rowData[0][4];
+				$tgllahir = $rowData[0][5];
+
+				$cekdata = $this->db->get_where('mhs', ['nim' => $nim]);
+				if ($cekdata->num_rows() > 0) {
+					++$jmlgagal;
+				} else {
+					$datasimpan = [
+						'nim' => $nim, 'namamahasiswa' => $nama,
+						'tempatlahir' => $tmplahir,
+						'tanggallahir' => $tgllahir,
+						'jeniskelamin' => $jenkel
+					];
+
+					$this->db->insert('mhs', $datasimpan);
+					++$jmlsukses;
+				}
+			}
+
+			echo 'Jumlah Data Yang Berhasil :<strong>' . $jmlsukses . '</strong><br>';
+			echo 'Jumlah Data Yang Gagal :<strong>' . $jmlgagal . '</strong><br>';
+		}
 	}
 }
